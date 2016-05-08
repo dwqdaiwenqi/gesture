beacd085dc6700d2bcb8e7296ad885bc5474458c
dwqdaiwenqi <1837512926@qq.com>
2016-05-08 19:00:07 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,3 @@
-
-
-
-
 module.exports = {
   entry: {
     //开发入口js
@@ -9,21 +5,11 @@ module.exports = {
     main2: './src/scripts/main2.js'
   }
   ,output: {
-    //开发js相对与这个目录
     path: __dirname+'/dist/scripts/',
-    //生成合并的js
-    //entry1.entry.js
-    //entry2.entry.js
     filename: '[name].js'
   }
   ,resolve: {
-    // 现在可以写 require('file') 代替 require('file.coffee')
-    // extensions: ['', '.js', '.jsx', '.css','.png','.jpg'] 
-    // ,alias: {
-    //    //alias依旧相对于开发的js。。。
-    //  // Dai: "http://localhost/default_web/js/Dai.js"
-    //  Dai: "../../dist/scripts/Dai.js"
-    // }
+
   }
  , module: {
     loaders: [
@@ -41,11 +27,9 @@ module.exports = {
       ,{test: /\.(png|jpg)$/, loader: 'url-loader'} // 内
     ]
   }
-  //例如是cdn形式加载的全局
+
   ,externals: {
-      // require("jquery") 是引用自外部模块的
-      // 对应全局变量 jQuery
-     // "_$": "Dai"
+
   }
 
 };
